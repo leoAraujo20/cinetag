aa1e38bceef152a105f5b98fcff11d3b4edafcf0
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-08-14 14:59:31 -0300

    Add Banner component and integrate it into Home page

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import styles from './Banner.module.css';
+
+function Banner({image}) {
+    return (
+        <div className={styles.banner} style={{ backgroundImage: `url(${image})` }}>
+        </div>
+    );
+}
+
+export default Banner;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header";
+import Banner from '@/components/Banner';
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header';
 
 function Home() {
-    return (
-        <>
-            <Header />
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <Banner image='/images/banner-home.png' />
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
