90befea4c312a9c991a0cb6006f031144364ef3b
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-08-14 15:05:19 -0300

    Add Title component and integrate it into Home page

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import styles from './Title.module.css';
+
+function Title({ children }) {
+    return (
+        <div className={styles.title}>
+            {children}
+        </div>
+    );
+}
+
+
+export default Title;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from '@/components/Banner';
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header';
+import Title from '@/components/Title';
 
 function Home() {
   return (
     <>
       <Header />
       <Banner image='/images/banner-home.png' />
+      <Title><h1>Um lugar para salvar seus filmes</h1></Title>
       <Footer />
     </>
   );
