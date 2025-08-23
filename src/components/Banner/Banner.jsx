e6190435649e395f6f6413b59214ccc8ebf307a5
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2025-08-23 16:09:10 -0300

    test

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,12 @@
-import styles from './Banner.module.css';
+import styles from "./Banner.module.css";
 
-function Banner({image}) {
-    return (
-        <div className={styles.banner} style={{ backgroundImage: `url(${image})` }}>
-        </div>
-    );
+function Banner({ image }) {
+  return (
+    <div
+      className={styles.banner}
+      style={{ backgroundImage: `url(${image})` }}
+    ></div>
+  );
 }
 
 export default Banner;
